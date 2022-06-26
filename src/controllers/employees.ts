e9a806dd73635c3ede6756c8eb90cbf7f257552e
wolkyura <yura02505@gmail.com>
2022-06-26 15:45:07 +0200

    fix: get employees

--- src/controllers/employees.ts
+++ src/controllers/employees.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 import { User } from '../models';
 
 const getEmployees = (req: Request, res: Response) => {
-  User.find({}).exec((err, employees) => {
+  User.find({ role: 'employee' }).exec((err, employees) => {
     if (err) {
       return res.status(500).send({ message: err });
     }
