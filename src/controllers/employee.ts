c27a78283f1267ef68109c6ad043dc45a24bd010
wolkyura <54850073+wolkyura@users.noreply.github.com>
2022-10-23 17:47:17 +0200

    feat: date filter (#5)

--- src/controllers/employee.ts
+++ src/controllers/employee.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
 
 import { Order } from '../models';
+import { stringToDate } from '../helpers/stringToDate';
+
+const DAY = 60 * 60 * 24 * 1000;
 
 const getEmployeeOrders = (req: Request, res: Response) => {
   const sortBy = (req.query.sortBy ?? 'creationDate') as string;
@@ -8,17 +11,26 @@ const getEmployeeOrders = (req: Request, res: Response) => {
 
   const startDate = (req.query.startDate ?? '') as string;
   const status = (req.query.status ?? '') as string;
-  
+
+  const dateStart = stringToDate(req.query.dateStart as string | undefined) || '';
+  const dateEnd = stringToDate(req.query.dateEnd as string | undefined) || '';
+
   const filter = {
     assignedEmployee: req.params.id,
     ...(startDate === 'true' && { startDate: { $ne: null } }),
     ...(startDate === 'false' && { startDate: null }),
-    ...(status ? { status } : { status: { $ne: 'completed' } })
+    ...(status ? { status } : { status: { $ne: 'completed' } }),
+    ...(dateStart && {
+      startDate: {
+        $gte: dateStart,
+        $lt: new Date((dateEnd || dateStart).getTime() + DAY),
+      },
+    }),
   };
 
-  const sorting = { 
-    [sortBy]: orderBy, 
-    ...(sortBy !== 'creationDate' && { creationDate: -1 }), 
+  const sorting = {
+    [sortBy]: orderBy,
+    ...(sortBy !== 'creationDate' && { creationDate: -1 }),
   } as Record<string, 1 | -1>;
 
   Order.find(filter).sort(sorting).populate('assignedEmployee').exec((error, orders) => {
