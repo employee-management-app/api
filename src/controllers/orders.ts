3b03c13b8139661696c33da30546e0ed65e16715
wolkyura <yura02505@gmail.com>
2023-02-18 16:22:36 +0100

    feat: reorganize filters

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -1,37 +1,74 @@
 import { endOfDay, startOfDay } from 'date-fns';
 import { Request, Response } from 'express';
-import mongoose from 'mongoose';
 
 import { sortNotCompletedOrders } from '../helpers/sortNotCompletedOrders';
 import { stringToDate } from '../helpers/stringToDate';
 import { Order } from '../models';
+import type { Order as OrderType } from '../types/order';
 
 const DAY = 60 * 60 * 24 * 1000;
 
-export const getOrders = (req: Request, res: Response) => {
-  const assignedEmployee = (req.query.assignedEmployee ?? '') as string;
-  const startDate = (req.query.startDate ?? '') as string;
-  const status = (req.query.status ?? '') as string;
+interface Query {
+  status?: OrderType['status'];
+  startDate?: string;
+  endDate?: string;
+  employee?: OrderType['_id'] | OrderType['_id'][];
+  stage?: OrderType['stage'] | OrderType['stage'][];
+  priority?: OrderType['priority'] | OrderType['priority'][];
+  type?: OrderType['type'] | OrderType['type'][];
+  unscheduled?: 'true' | 'false';
+  unassigned?: 'true' | 'false';
+}
 
-  const dateStart = stringToDate(req.query.dateStart as string | undefined) || '';
-  const dateEnd = stringToDate(req.query.dateEnd as string | undefined) || '';
+export const getOrders = (req: Request<any, any, any, Query>, res: Response) => {
+  const { status, unscheduled, unassigned } = req.query;
+  const startDate = stringToDate(req.query.startDate);
+  const endDate = stringToDate(req.query.endDate);
+  const employee = req.query.employee ? [req.query.employee].flat() : null;
+  const stage = req.query.stage ? [req.query.stage].flat() : null;
+  const priority = req.query.priority ? [req.query.priority].flat() : null;
+  const type = req.query.type ? [req.query.type].flat() : null;
+  const returnUnscheduled = unscheduled ? unscheduled === 'true' : false;
+  const returnUnassigned =  unassigned ? unassigned === 'true' : false;
 
-  const filter = {
-    ...(assignedEmployee === 'true' && { assignedEmployee: { $ne: null } }),
-    ...(assignedEmployee === 'false' && { assignedEmployee: null }),
-    ...(mongoose.Types.ObjectId.isValid(assignedEmployee) && { assignedEmployee }),
-    ...(startDate === 'true' && !dateStart && { startDate: { $ne: null } }),
-    ...(startDate === 'false' && !dateStart && { startDate: null }),
-    ...(dateStart && {
-      startDate: {
-        $gte: dateStart,
-        $lt: new Date((dateEnd || dateStart).getTime() + DAY),
-      },
-    }),
-    ...(status ? { status } : { status: { $ne: 'completed' } })
+  const orWrapper = (query: Record<string, object>) => {
+    if (returnUnscheduled && returnUnassigned) {
+      return { $or: [query, { assignedEmployee: null }, { startDate: null }] };
+    }
+
+    if (returnUnscheduled) {
+      return { $or: [query, { startDate: null }] };
+
+    }
+
+    if (returnUnassigned) {
+      return { $or: [query, { assignedEmployee: null }] };
+    }
+
+    return query;
   };
 
-  Order.find(filter).populate('assignedEmployee').exec((error, orders) => {
+  const filter = {
+    ...(status ? { status } : { status: { $ne: 'completed' } }),
+    ...orWrapper({
+      ...(startDate && {
+        startDate: {
+          $gte: startDate,
+          $lt: new Date((endDate ?? startDate).getTime() + DAY),
+        },
+      }),
+      ...(employee && { assignedEmployee: { $in: employee } }),
+      ...(stage && { stage: { $in: stage } }),
+      ...(priority && { priority: { $in: priority } }),
+      ...(type && { type: { $in: type } }),
+    }),
+  };
+
+  const sort = status === 'completed'
+    ? { completedDate: -1, _id: -1 } as const
+    : undefined;
+
+  Order.find(filter).populate('assignedEmployee').sort(sort).exec((error, orders) => {
     if (error) {
       return res.status(500).send(error);
     }
