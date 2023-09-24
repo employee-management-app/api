6cc2d645e51fc03309e17a6213f6cfc43fb1f89d
wolkyura <yura02505@gmail.com>
2023-09-24 11:59:16 +0200

    feat: allow order creation with provided company id

---
+++ src/middlewares/verifyCompanyIdOrToken.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { Company } from '../models/company';
+import { verifyToken } from './verifyToken';
+
+export const verifyCompanyIdOrToken = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers['authorization']?.replace('Token ', '');
+  const { companyId } = req.body;
+
+  if (!token && !companyId) {
+    return res.status(403).send({ message: 'No token or company id provided!' });
+  }
+
+  if (token) {
+    return verifyToken(req, res, next);
+  }
+
+  Company.findById(companyId)
+    .then((company) => {
+      if (!company) {
+        return res.status(404).send({ message: 'Company with provided id doesn\'t exist' });
+      }
+
+      res.locals.companyId = companyId;
+
+      next();
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+};
--- src/routes/order.ts
+++ src/routes/order.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import { createOrder, deleteOrder, getOrder, removeFile, updateOrder, uploadFile } from '../controllers/order';
+import { verifyCompanyIdOrToken } from '../middlewares/verifyCompanyIdOrToken';
 import { verifyToken } from '../middlewares/verifyToken';
 
 const upload = multer({ dest: '/tmp' });
 
 export default (router: Router) => {
   router.get('/order/:id', [verifyToken], getOrder);
-  router.post('/order', [verifyToken], createOrder);
+  router.post('/order', [verifyCompanyIdOrToken], createOrder);
   router.patch('/order/:id', [verifyToken], updateOrder);
   router.delete('/order/:id', [verifyToken], deleteOrder);
   router.post('/order/:id/file', [verifyToken, upload.single('file')], uploadFile);
